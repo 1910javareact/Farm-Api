fd0c06261f265599c39a21ec47134a1d1b9b41d0
Kenneth Davis <kennethdavis391@gmail.com>
2019-12-02 15:19:59 -0500

    fixed

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,11 +1,4 @@
 import { Pool } from 'pg';
-console.log({
-    user: process.env['DAIRYLAND_USERNAME'],
-    host: process.env['DAIRYLAND_HOST'],
-    database: process.env['DAIRYLAND_DATABASE'],
-    port: 5432,
-    max: 5,
-});
 
 export const connectionPool: Pool = new Pool({
     user: process.env['DAIRYLAND_USERNAME'],
