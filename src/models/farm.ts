dd7cdacd29b94cd3b4452f2fd45f17d3542de364
Zia pmr <gernersnc@gmail.com>
2019-12-02 15:50:28 -0500

    changes

---
+++ src/models/farm.ts
@@ -0,0 +1,15 @@
+export class Farm {
+    farm_id: number
+    location: string
+    farm_name: string
+    farm_age: number
+    agri_type: string
+    revenue: number
+    constructor (farm_id: number,   location: string, farm_name: string, farm_age: number, agri_type: string, revenue: number ){
+        this.location = location,
+        this.farm_name = farm_name,
+        this.farm_age = farm_age,
+        this.agri_type = agri_type,
+        this.revenue = revenue
+    }
+}
