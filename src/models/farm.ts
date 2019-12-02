b186e0100b23c698b8f10c0a2aced81d4c578ab0
TKSun21 <kietnguyen2016@gmail.com>
2019-12-02 15:51:10 -0500

    farm-to-farm udate

--- src/models/farm.ts
+++ src/models/farm.ts
@@ -1,19 +1,6 @@
 export class Farm {
     farm_id: number
     location: string
-<<<<<<< HEAD
-    farm_name: number
-    farm_age: number
-    agri_type: string
-    revenue: number
-    constructor (farm_id: number, location: string, farm_name: number, farm_age: number, agri_type: string, revenue: number){
-        this.farm_id = farm_id;
-        this.location = location;
-        this.farm_name = farm_name;
-        this.farm_age = farm_age;
-        this.agri_type = agri_type;
-        this.revenue = revenue;
-=======
     farm_name: string
     farm_age: number
     agri_type: string
@@ -25,6 +12,5 @@ export class Farm {
         this.farm_age = farm_age,
         this.agri_type = agri_type,
         this.revenue = revenue
->>>>>>> 000ee0d718d6d671077dfe0eee0c6dea411dacea
     }
 }
