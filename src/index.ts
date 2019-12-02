97c358d36e36d09d6f753e62bf66d0f1c173fe29
Kenneth Davis <kennethdavis391@gmail.com>
2019-12-02 15:25:44 -0500

    router in place

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import bodyparser from 'body-parser';
+import { workerRouter } from './routers/worker-router';
 
 const app = express()
 
 app.use(bodyparser.json());
+
+app.use('/workers', workerRouter)
 
 app.listen(9000, ()=>{
     console.log("App is Alive");
