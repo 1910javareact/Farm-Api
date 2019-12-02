1feb8cc07e231a7dc2db3cba9997b1cfa9d6ab39
mahmud <mahmudskarim@gmail.com>
2019-12-02 14:19:57 -0500

    Basic folders created

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import bodyparser from 'body-parser';
 
 const app = express()
+
+app.use(bodyparser.json());
 
 app.listen(9000, ()=>{
     console.log("App is Alive");
